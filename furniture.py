29cecf8a667c3847ae76aa0a220c1b3c5c465f14
Nika <nika.mohseni85@gmail.com>
2024-10-07 23:39:14 +0330

    Delete furniture.py

--- furniture.py
+++
@@ -1,7 +0,0 @@
-from Product_Project.not_electrical import NotElectrical
-
-
-class Furniture(NotElectrical):
-    def __init__(self, name, price, weight, capacity):
-        super().__init__(name, price, weight)
-        self.capacity = capacity
