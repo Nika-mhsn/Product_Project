80369b04f90521dbd39b8ed78dbecb68085aa203
Nika <nika.mohseni85@gmail.com>
2024-10-07 23:42:56 +0330

    Delete laptop.py

--- laptop.py
+++
@@ -1,8 +0,0 @@
-from Product_Project.electrical import Electrical
-
-
-class LapTop(Electrical):
-    def __init__(self, name, price, vol, cpu, ram):
-        super().__init__(name, price, vol)
-        self.cpu = cpu
-        self.ram = ram
